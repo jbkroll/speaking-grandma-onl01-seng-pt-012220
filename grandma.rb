394e6d0a1c0bcb56e42129a1064e89018ef44d61
jenniferbantakroll <jenniferbantakroll@gmail.com>
2020-02-01 18:10:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrse.upcase
+  elsif phrase.upcase
     "NO, NOT SINCE 1938!"
   end
 end
