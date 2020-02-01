ba8c2e0169d52a2813855f8f7e843713b741ebbb
jenniferbantakroll <jenniferbantakroll@gmail.com>
2020-02-01 17:54:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-if phrase 
+if phrase == "I LOVE YOU GRANDMA!"
+  "ILOVE YOU TOO PUMPKIN!"
+  
   
   !'I LOVE YOU GRANDMA'
   puts 'HUH? Speak up, Sonny!'
