114dc48018367ed0635d0a17d18c03f7e205c1c0
jenniferbantakroll <jenniferbantakroll@gmail.com>
2020-02-01 18:18:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-  
+    
   else
-    "NO, NOT SINCE 1938!"
+    
     "HUH?! SPEAK UP, SONNY!"
   end
 end
